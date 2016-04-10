fb426dafc271569b151182fb5753be67905afb25
Billtholomew <wpb47@cornell.edu>
2016-04-10 09:11:56 -0400

    Initial commit for Visual Studio template for opencv projects

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include <iostream>
+#include <ctime>
+#include <omp.h>
+
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+using namespace cv;
+
+int main(int argc, const char** argv)
+{
+}
